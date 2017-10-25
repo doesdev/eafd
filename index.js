7ce44633b5bf53d4a2fa50c8da6718af0765f9c2
Andrew Carpenter <andrew@audioinhd.com>
2017-10-24 22:58:05 -0400

    I promise I tested the cli this time first :\

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 const tts = require('google-tts-api')
 const { get } = require('axios')
 const { escape } = require('validator')
+const { resolve } = require('path')
 const { readFileSync } = require('fs')
-const eafd = readFileSync(`audio/eafd.mp3`)
+const eafd = readFileSync(resolve(__dirname, `audio`, `eafd.mp3`))
 
 module.exports = async (name) => {
   let url = await tts(escape(name), `en`, 1)
